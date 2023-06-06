08c5b2e104b2efba9c1452c2bfab5b431d156cd3
Anandprabhu530 <anandprabhu530@gmail.com>
2023-06-06 17:19:46 +0530

    Added insertion sort

--- Sorting/insertion_sort.java
+++ Sorting/insertion_sort.java
@@ -1,3 +1,25 @@
-public class insertion_sort {
-    
+import java.util.*;
+class main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements: ");
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }else{
+                    break;
+                }
+            }
+        }
+        System.out.println("The sorted Array is: " + Arrays.toString(arr));
+    }
 }
