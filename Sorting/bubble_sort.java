5e05b1f17303230b9b3c1b408c1f64c4cf68d0c6
Anandprabhu530 <anandprabhu530@gmail.com>
2023-06-06 16:49:30 +0530

    Added bubble sort

--- Sorting/bubble_sort.java
+++ Sorting/bubble_sort.java
@@ -11,17 +11,14 @@ class main{
             arr[i] = sc.nextInt();
         }
         for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]>arr[j]){
-                    int temp  =arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
                 }
             }
         }
-        System.out.println("The sorted Array is: ");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i] + " ");
-        }
+        System.out.println("The sorted Array is: " + Arrays.toString(arr));
     }
 }
