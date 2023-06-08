eebce50042c07f0fcb826c059da617f85fd2f219
Anandprabhu530 <anandprabhu530@gmail.com>
2023-06-09 00:08:34 +0530

    Added wrapper

---
+++ Oop/wrapper.java
@@ -0,0 +1,12 @@
+package Java_Practice.Oop;
+
+class wrapper {
+    public static void main(String[] args) {
+        System.out.println("Wrapper class");
+        Integer a = 10;
+        // int a = 10;
+        System.out.println(a.hashCode());
+        Character b = 'r';
+        System.out.println(Character.getNumericValue('z'));
+    }
+}
