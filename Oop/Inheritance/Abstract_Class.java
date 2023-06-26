d41b0c8e369e23e65d38459ceebdce6d932245ce
Anandprabhu530 <anandprabhu530@gmail.com>
2023-06-26 15:44:32 +0530

    Changes in Abstract

--- Oop/Inheritance/Abstract_Class.java
+++ Oop/Inheritance/Abstract_Class.java
@@ -2,18 +2,24 @@ package Java_Practice.Oop.Inheritance;
 
 public class Abstract_Class {
     public static void main(String[] args) {
-        support_class obj = new support_class() {
-            @Override
-            void fly() {
-                System.out.println("Iam Sleeping");
-            }
-
-            void sleep() {
-                System.out.println("Iam Sleeping inside main class");
-            }
-        };
-        obj.fly();
+        ans obj = new ans();
+        obj.eat();
         obj.sleep();
+        obj.fly();
+    }
+}
+
+class ans extends support_class {
+    void eat() {
+        System.out.println("Iam inside ans class");
+    }
+
+    void sleep() {
+        System.out.println("Iam sleeping inside ans class");
+    }
+
+    void fly() {
+        System.out.println("Iam flying in ans class");
     }
 }
 
@@ -21,6 +27,6 @@ abstract class support_class {
     abstract void fly();
 
     void sleep() {
-        System.out.print("Iam Sleeping inside abstarct class");
+        System.out.println("Iam Sleeping inside abstarct class");
     }
 }
